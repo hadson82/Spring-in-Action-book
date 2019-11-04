aa795a613280e3e2c8a3b181f49e4fdeb5a35979
Pavel <rogozhin.p@yandex.ru>
2019-11-04 17:58:32 +0300

    2chapter done, start working with spring git repo, spring framework documentation

--- taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -3,11 +3,35 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TacoCloudApplication {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication // <1>
+public class TacoCloudApplication implements CommandLineRunner {
+
+	@Autowired
+	private ApplicationContext appContext;
 
 	public static void main(String[] args) {
-		SpringApplication.run(TacoCloudApplication.class, args);
+		SpringApplication.run(TacoCloudApplication.class, args); // <2>
+	}
+	
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		String[] beans = appContext.getBeanDefinitionNames();
+		Arrays.sort(beans);
+		for (String bean : beans) {
+			System.out.println(bean);
+		}
+
 	}
 
 }
