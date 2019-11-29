6a2ca6db1e10f0581149092ced4f37d3448871b9
Pavel <rogozhin.p@yandex.ru>
2019-11-30 02:37:25 +0300

    checkboxes displayed

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -57,7 +57,8 @@ public class DesignTacoController {
 
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+			model.addAttribute(type.toString().toLowerCase(), 
+					filterByType(ingredients, type));
 		}
 
 		return "design";
